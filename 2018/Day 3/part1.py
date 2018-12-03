62c385c06b83455466d54e2dd6bff779b6a54e4d
Suhail <suhailm0608@gmail.com>
2018-12-03 05:22:41 +0000

    Day 3

---
+++ 2018/Day 3/part1.py
@@ -0,0 +1,19 @@
+inputt = open("Day 3.txt","r").readlines()
+
+coordinates_used = {}
+
+for x in inputt:
+    top_corner = x.split("@ ")[1].split(":")[0]
+    coordinates = [int(y) for y in top_corner.split(",")]
+    length = int(x.split(": ")[1].split("x")[0])
+    width = int(x.split("x")[1])
+    for l in range(0, length):
+        for w in range(0, width):
+            cos = [coordinates[0]+l,coordinates[1]+w]
+            if "{},{}".format(cos[0],cos[1]) in coordinates_used:
+                coordinates_used["{},{}".format(cos[0],cos[1])]+=1
+            else:
+                coordinates_used["{},{}".format(cos[0],cos[1])] = 1
+    
+thing = [x for x in coordinates_used if coordinates_used[x] > 1]
+print(len(thing))
---
+++ 2018/Day 3/part2.py
@@ -0,0 +1,27 @@
+inputt = open("Day 3.txt","r").readlines()
+
+coordinates_used = {}
+exclusive = []
+
+
+for x in inputt:
+    top_corner = x.split("@ ")[1].split(":")[0]
+    coordinates = [int(y) for y in top_corner.split(",")]
+    length = int(x.split(": ")[1].split("x")[0])
+    width = int(x.split("x")[1])
+    id = int(x.split("#")[1].split(" ")[0])
+    overlap = False
+    for l in range(0, length):
+        for w in range(0, width):
+            cos = [coordinates[0]+l,coordinates[1]+w]
+            string = "{},{}".format(cos[0],cos[1])
+            if string in coordinates_used:
+                if coordinates_used[string] in exclusive:
+                    exclusive.remove(coordinates_used[string])
+                overlap = True
+            else:
+                coordinates_used[string] = id
+    if not overlap:
+        exclusive.append(id)
+
+print(exclusive)
